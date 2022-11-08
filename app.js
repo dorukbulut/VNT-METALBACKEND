bde54000e91c7c0500a5cbb072cb4be5ec8e10c0
Doruk Bulut <dorukbulutinfo@gmail.com>
2022-11-09 00:31:31 +0300

    Finish Config

---
+++ app.js
@@ -0,0 +1,38 @@
+//imports
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors'
+import router from "./routes/index.js";
+import db from "./config/database.js";
+import cookieParser from 'cookie-parser';
+//dotenv config
+dotenv.config()
+
+/*express config*/
+const app = express();
+app.use(cors({
+  origin : process.env.FRONTEND,
+  credentials : true
+}))
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//import route
+app.use(router)
+
+//Connect to db
+
+try {
+  await db.authenticate();
+  console.log('Connection has been established successfully.');
+}
+
+catch (error) {
+  console.error('Unable to connect to the database:', error);
+};
+
+
+
+
+export default app;
---
+++ config/database.js
@@ -0,0 +1,16 @@
+import Sequelize from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT,
+        dialect : "postgres"
+
+    }
+)
+
+export default db;
