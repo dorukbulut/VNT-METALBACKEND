6d397e3cbbd316ddf29adec22ddaa4b980ff02cf
doruk <dorukbulutinfo@gmail.com>
2023-01-16 11:39:22 +0300

    a

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use("/api",router)
 
 try {
   await db.authenticate();
-  await db.sync({alter:true});
+  await db.sync({force:true});
   console.log('Connection has been established successfully.');
 }
 
