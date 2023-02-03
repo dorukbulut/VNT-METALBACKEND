f1750532a723b61505e116d9ffe0fbfe52f2a9ce
doruk <dorukbulutinfo@gmail.com>
2023-02-03 11:14:14 +0300

    sale confirmation revisions

--- models/saleConfirmation.model.js
+++ models/saleConfirmation.model.js
@@ -1,74 +1,71 @@
 import db from "../config/database.js";
-import Sequelize  from "sequelize";
-
+import Sequelize from "sequelize";
 
 const SaleConfirmation = db.define(
-    "sale_confirmation",
-    {
-        sale_ID : {
-            type : Sequelize.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            primaryKey : true,
-            allowNull : false,
-        },
-        OrderDate : {
-            type : Sequelize.STRING,
-            allowNull : false,
-        },
-
-        deliveryDate : {
-            type : Sequelize.STRING,
-            allowNull : false,
-        },
-
-        customerReference : {
-            type : Sequelize.STRING,
-            
-        },
-        specialOffers : {
-            type: Sequelize.STRING,
-        },
-        reference : {
-            type : Sequelize.STRING,
-            allowNull : false,
-        },
-        revision : {
-            type : Sequelize.INTEGER,
-            
-        },
-
-        description : {
-            type  : Sequelize.STRING,
-        },
-
-        year : {
-            type : Sequelize.BIGINT,
-        },
-
-        month : {
-            type : Sequelize.BIGINT,
-        },
-
-        day : {
-            type : Sequelize.BIGINT,
-        },
-
-        package : {
-            type : Sequelize.BOOLEAN
-        }
-
-        
-
-
+  "sale_confirmation",
+  {
+    sale_ID: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    OrderDate: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
 
-    {
-        
-       
-        timestamps : false,
-       
-    }
-); 
+    deliveryDate: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
 
+    customerReference: {
+      type: Sequelize.STRING,
+    },
+    specialOffers: {
+      type: Sequelize.STRING,
+    },
+    reference: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    revision: {
+      type: Sequelize.INTEGER,
+    },
+
+    description: {
+      type: Sequelize.STRING,
+    },
+
+    year: {
+      type: Sequelize.BIGINT,
+    },
+
+    month: {
+      type: Sequelize.BIGINT,
+    },
+
+    day: {
+      type: Sequelize.BIGINT,
+    },
+
+    package: {
+      type: Sequelize.BOOLEAN,
+    },
+
+    company: {
+      type: Sequelize.STRING,
+    },
+
+    language: {
+      type: Sequelize.STRING,
+    },
+  },
+
+  {
+    timestamps: false,
+  }
+);
 
 export default SaleConfirmation;
