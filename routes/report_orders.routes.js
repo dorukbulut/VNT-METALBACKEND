5c43162f6e506a26947fa24afa113b390f06fcda
Doruk Bulut <dorukbulutinfo@gmail.com>
2023-07-30 01:21:19 +0300

    Report Production

--- routes/report_orders.routes.js
+++ routes/report_orders.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 const router = express.Router();
+import ReportOrdersService from "../controllers/report_orders.controller.js"
 
+router.post("/customer-report", ReportOrdersService.customerReport)
 
 export default router
